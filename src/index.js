13d6688e4819f13242a090910b65db4d93fdbc82
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-05-18 16:18:02 +0100

    HTML, CSS and JS HTML for forecast

--- src/index.js
+++ src/index.js
@@ -68,4 +68,30 @@ function cityForm(event) {
 let searchElement = document.querySelector("#city-search-form");
 searchElement.addEventListener("submit", cityForm);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+
+    let currentForecast = document.querySelector("#forecast");
+    currentForecast.innerHTML = forecastHtml;
+  });
+}
+
 searchCity("Paris");
+displayForecast();
